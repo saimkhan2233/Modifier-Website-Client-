f64582853dff7ef83dda3b9ca9ccdb2496a2ede6
Saim khan <msaimkhan2233@gmail.com>
2026-05-15 21:01:49 +0500

    Refactor index.js to render React application

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-export { default } from "./ModifierWebsite";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './ModifierWebsite.jsx'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
